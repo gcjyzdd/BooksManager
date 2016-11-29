0011a1e6d5e63ec3ebe9c7e3a6c70efe4080c30f
Changjie <changjieguan@gmail.com>
2016-11-29 12:55:50 +0100

    revised review class

--- review_stars.py
+++ review_stars.py
@@ -3,28 +3,36 @@
 
 import Tkinter as tk
 
-class review_stars():
+
+class review_stars(tk.Frame):
     '''
     create 5 stars showing the review score of a book,
     the user can also set review score for a book 
     '''
-    def __init__(self,master):
+    def __init__(self,master,**kw):
         
+        #tk.Frame.__init__(self, master,kw)
+        apply(tk.Frame.__init__,(self,master),kw)
         self.stars=[]
+        
+        #set default score
+        self.score=tk.IntVar()
+        
+        # set bind functions for each star
+        fl=map(lambda i:( lambda event:(self.score.set(i+1))), list(range(5)))
         for i in range(5):
             self.stars.append(tk.Canvas(master,width=20,height=20))
             self.stars[i].grid(row=0,column=i,sticky=(tk.N,tk.S,tk.W,tk.E))
-            # set bind here to set score and write to database
-            pass
-            
+            self.stars[i].bind('<Button-1>',fl[i])    
+                    
+    
         label=tk.Label(master,text='Reivew')
         label.grid(row=0,column=5,columnspan=3,sticky=(tk.N,tk.S,tk.W,tk.E),padx=10)
         self.label=label
-        
-        
+                
         self.update()
         
-        self.draw(0)
+        self.draw(self.score.get())
         
     def draw(self,score):
         '''
@@ -46,7 +54,8 @@ class review_stars():
             self.stars[i].update()
         self.label.update()
 
-
+        
+    
 class FBtn():
     '''
     four buttons, just a test
@@ -57,19 +66,28 @@ class FBtn():
             self.btns.append(tk.Button(master,text="button"+str(i)))        
             self.btns[i].grid(row=0,column=i)
 
-if __name__=='__main__':
+def poll():
+    RV.after(200, poll)
+    print 'score',RV.score.get()
+    
+    
+if __name__=='__main__':    
+    
     root=tk.Tk()
     root.title('stars')
     
-    FmLeft=tk.Frame(root,bg='white',width=800,height=100,padx=5,pady=5)
-    FmLeft.grid(row=0,column=0)
-    FmLeft.update()
+    FmUp=tk.Frame(root,bg='white',width=800,height=100,padx=5,pady=5)
+    FmUp.grid(row=0,column=0)
+    FmUp.update()
     
     FmBottom=tk.Frame(root,width=800,height=100)
     FmBottom.grid(row=1,column=0)
     FBtn(FmBottom)
+        
+    RV=review_stars(FmUp)
     
-    print 'height1: ',FmLeft.winfo_height()
-    review_stars(FmLeft)
+    poll()
+
     root.mainloop()
-            
+
+    
